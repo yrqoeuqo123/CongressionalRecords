024e96ecdfb0705fbefce8361fc7db6c62350ef2
Tianyi Zhang <zhangtianyi975@gmail.com>
2023-11-22 00:11:25 -0500

    add

--- scripts/Text Mining/Preprocessing_for_text_mining/preprocessing_multiprocessing.py
+++ scripts/Text Mining/Preprocessing_for_text_mining/preprocessing_multiprocessing.py
@@ -7,13 +7,33 @@ from nltk.stem import PorterStemmer
 from tqdm import tqdm
 from multiprocessing import Pool
 
+'''
+Be Careful: This script uses multiprocessing to speed up the preprocessing process. This might burden your CPU if your hardware is not powerful enough.
+If you do not want to use multiprocessing, you could use the script "preprocessing.py" instead.
+
+This script preprocesses the text files extracted from the PDFs.
+Preprocessing steps:
+    1. Convert to lowercase
+    2. Remove punctuation
+    3. Tokenize text
+    4. Remove stopwords and numbers
+    5. (Optional) Stem words
+
+Preprocessing is always important, but especially so for topic modeling.
+
+The files are already saved and you could find them in the folder "Processed_Text" on my GitHub.
+Be careful the processed texts on the github are stemmed. 
+If you do not want to stem the texts, you could change the use_stemmer to false.
+'''
+
+
 # Download stopwords from NLTK
 nltk.download('punkt')
 nltk.download('stopwords')
 
 # Set the directories
-input_directory = r'C:\Users\Tianyi Zhang\Desktop\Lab\Extracted_Text'
-output_directory = r'C:\Users\Tianyi Zhang\Desktop\Lab\Processed_Text'
+input_directory = r'YOUR_DIRECTORY\Lab\Text'
+output_directory = r'YOUR_DIRECTORY\Lab\Text\Processed'
 
 # Check if output directory exists, if not, create it
 if not os.path.exists(output_directory):
